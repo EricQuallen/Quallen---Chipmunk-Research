10ecbf9975932021e52a64777e19719b0b1d39b9
Joost Huizinga <joost.hui@gmail.com>
2021-06-13 12:31:30 -0700

    Added code for controlling two motors with two pressure pads.

--- combined_test.py
+++ combined_test.py
@@ -1,4 +1,3 @@
-import os
 import time
 import busio
 import digitalio
@@ -7,10 +6,21 @@ import adafruit_mcp3xxx.mcp3008 as MCP
 from adafruit_mcp3xxx.analog_in import AnalogIn
 from adafruit_motorkit import MotorKit
 
-kit = MotorKit(adress=96)
+# create the spi bus
+spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
+cs = digitalio.DigitalInOut(board.D22)
+mcp = MCP.MCP3008(spi, cs)
+chan0 = AnalogIn(mcp, MCP.P0)
+chan1 = AnalogIn(mcp, MCP.P1)
+
+kit = MotorKit(address=96)
 kit2 = MotorKit(address=97)
 
-for i in range(100):
-    kit.stepper1.onestep()
-    kit.stepper2.onestep()
-    kit2.stepper3.onestep()
+while True:
+    if chan0.value > 100:
+        for i in range(1000):
+            kit.stepper1.onestep()
+    elif chan1.value > 100:
+        for i in range(1000):
+            kit2.stepper1.onestep()
+    time.sleep(0.05)
