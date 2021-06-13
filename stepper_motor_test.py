71f1f1731f46c444769c19eea04d6337684505f8
Joost Huizinga <joost.hui@gmail.com>
2021-06-13 10:08:48 -0700

    Manually creating the i2c.

--- stepper_motor_test.py
+++ stepper_motor_test.py
@@ -1,10 +1,24 @@
 from adafruit_motorkit import MotorKit
+from adafruit_blinka.agnostic import board_id, detector
 import board
 import busio
 
-print("SCL:", board.SCL)
-print("SDA:", board.SDA)
+if detector.board.any_embedded_linux:
+    from adafruit_blinka.microcontroller.generic_linux.i2c import I2C as _I2C
+elif detector.board.ftdi_ft2232h:
+    from adafruit_blinka.microcontroller.ftdi_mpsse.mpsse.i2c import I2C as _I2C
+else:
+    from adafruit_blinka.microcontroller.generic_micropython.i2c import (
+        I2C as _I2C,
+    )
 
+_i2c = _I2C(1, mode=_I2C.MASTER, baudrate=100000)
+
+
+print(_i2c)
+# print("SCL:", board.SCL)
+# print("SDA:", board.SDA)
+#
 # i2c = busio.I2C(board.SCL, board.SDA)
 # print(i2c.scan())
 # i2c.deinit()
