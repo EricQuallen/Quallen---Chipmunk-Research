342b2eecdb6e7060f5b485624ad1f4af2ff79a36
Joost Huizinga <joost.hui@gmail.com>
2021-06-13 09:31:31 -0700

    Set i2c port.

--- combined_test.py
+++ combined_test.py
@@ -7,8 +7,10 @@ import adafruit_mcp3xxx.mcp3008 as MCP
 from adafruit_mcp3xxx.analog_in import AnalogIn
 from adafruit_motorkit import MotorKit
 
-kit = MotorKit()
+kit = MotorKit(adress=96)
+kit2 = MotorKit(address=97)
 
 for i in range(100):
     kit.stepper1.onestep()
     kit.stepper2.onestep()
+    kit2.stepper3.onestep()
--- stepper_motor_test.py
+++ stepper_motor_test.py
@@ -1,6 +1,6 @@
 from adafruit_motorkit import MotorKit
 
-kit = MotorKit()
+kit = MotorKit(i2c=1)
 
 for i in range(100):
     kit.stepper1.onestep()
