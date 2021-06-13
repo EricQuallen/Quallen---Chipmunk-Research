903ef4834d9ec1aba90d26eb03fbb62f19c1a5fc
Joost Huizinga <joost.hui@gmail.com>
2021-06-13 10:29:01 -0700

    Reverted back to basics.

--- stepper_motor_test.py
+++ stepper_motor_test.py
@@ -3,24 +3,24 @@ from adafruit_blinka.agnostic import board_id, detector
 import board
 import busio
 
-if detector.board.any_embedded_linux:
-    from adafruit_blinka.microcontroller.generic_linux.i2c import I2C as _I2C
-    print("Embedded linux")
-elif detector.board.ftdi_ft2232h:
-    from adafruit_blinka.microcontroller.ftdi_mpsse.mpsse.i2c import I2C as _I2C
-    print("ftdi_mpsse")
-else:
-    from adafruit_blinka.microcontroller.generic_micropython.i2c import (
-        I2C as _I2C,
-    )
-    print("Generic microcontroller")
-
-print("Chip id:", detector.chip.id)
-
-_i2c = _I2C(10, mode=_I2C.MASTER, baudrate=100000)
-
-
-print(_i2c)
+# if detector.board.any_embedded_linux:
+#     from adafruit_blinka.microcontroller.generic_linux.i2c import I2C as _I2C
+#     print("Embedded linux")
+# elif detector.board.ftdi_ft2232h:
+#     from adafruit_blinka.microcontroller.ftdi_mpsse.mpsse.i2c import I2C as _I2C
+#     print("ftdi_mpsse")
+# else:
+#     from adafruit_blinka.microcontroller.generic_micropython.i2c import (
+#         I2C as _I2C,
+#     )
+#     print("Generic microcontroller")
+#
+# print("Chip id:", detector.chip.id)
+#
+# _i2c = _I2C(10, mode=_I2C.MASTER, baudrate=100000)
+#
+#
+# print(_i2c)
 # print("SCL:", board.SCL)
 # print("SDA:", board.SDA)
 #
@@ -29,7 +29,7 @@ print(_i2c)
 # i2c.deinit()
 #
 #
-# kit = MotorKit(i2c=board.I2C())
-#
-# for i in range(100):
-#     kit.stepper1.onestep()
+kit = MotorKit()
+
+for i in range(100):
+    kit.stepper1.onestep()
