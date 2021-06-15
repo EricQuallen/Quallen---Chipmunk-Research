6e7f38b58265ebd0527c5ab25a851a70fc71c9ef
Eric Quallen <80923246+EricQuallen@users.noreply.github.com>
2021-06-15 10:37:32 -0600

    Update stepper_motor_test.py

--- stepper_motor_test.py
+++ stepper_motor_test.py
@@ -33,7 +33,7 @@ print(i2c.scan())
 kit1 = MotorKit(address=96)
 
 for i in range(145):
-    #kit1.stepper1.onestep()
+    kit1.stepper1.onestep()
     # kit1.stepper2.onestep()
 
 for i in range(145):
