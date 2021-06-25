28556776a235ffd451e4bbc7658a15b07e793d2f
Eric Quallen <80923246+EricQuallen@users.noreply.github.com>
2021-06-25 15:56:23 -0600

    Update stepper_motor_test.py

--- stepper_motor_test.py
+++ stepper_motor_test.py
@@ -36,7 +36,7 @@ kit1 = MotorKit(address=96)
    # kit1.stepper1.onestep()
     # kit1.stepper2.onestep()
 
-for i in range(150):
+for i in range(148):
     kit1.stepper2.onestep()
 
 #kit2 = MotorKit(address=97)
