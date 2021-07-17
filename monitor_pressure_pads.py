776a524ff079d073be67067b62e332e4f9c7ee06
Joost Huizinga <joost@openai.com>
2021-07-17 13:25:12 -0700

    Calibration code

--- monitor_pressure_pads.py
+++ monitor_pressure_pads.py
@@ -6,6 +6,8 @@ import digitalio
 import board
 import adafruit_mcp3xxx.mcp3008 as MCP
 from adafruit_mcp3xxx.analog_in import AnalogIn
+import numpy as np
+
 
 with open(os.path.join(os.path.dirname(__file__), "pin_mapping.json")) as fh:
     pin_settings = json.load(fh)
@@ -19,11 +21,23 @@ left_pressure_pad = AnalogIn(mcp, pin_settings["left_pressure_pad"])
 middle_pressure_pad = AnalogIn(mcp, pin_settings["middle_pressure_pad"])
 right_pressure_pad = AnalogIn(mcp, pin_settings["right_pressure_pad"])
 
+left_values = []
+middle_values = []
+right_values = []
+
 with open(os.path.join(os.path.dirname(__file__), "values.csv"), 'w') as fh:
     for i in range(1000):
         right_value = right_pressure_pad.value
         middle_value = middle_pressure_pad.value
         left_value = left_pressure_pad.value
+        left_values.append(left_value)
+        right_values.append(right_value)
+        left_values.append(left_value)
         fh.write(f"{left_value},{middle_value},{right_value}\n")
         print(f"{i} registered values; left: {left_value}, middle {middle_value}, right {right_value}")
+
         time.sleep(0.01)
+
+print("Left mean:", np.mean(np.array(left_values)), "var:", np.var(np.array(left_values)))
+print("Middle mean:", np.mean(np.array(middle_values)), "var:", np.var(np.array(middle_values)))
+print("Right mean:", np.mean(np.array(right_values)), "var:", np.var(np.array(right_values)))
