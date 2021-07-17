24e47016abd380f373fd856f9ff6fa68ed54f124
Joost Huizinga <joost@openai.com>
2021-07-17 14:14:07 -0700

    Calibrated middle pressure pad at 30

--- chipmunk.py
+++ chipmunk.py
@@ -270,7 +270,7 @@ class PressurePads:
         # 128 -> 50g
         # 192-256 -> 70g
         self.left_threshold = 120
-        self.middle_threshold = 4
+        self.middle_threshold = 30
         self.right_threshold = 30
         # self.threshold = 200
 
